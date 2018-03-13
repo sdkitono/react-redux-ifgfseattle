c3214ccc0a1c60434b67216109cce20dd62b0d25
Evan Lee <evan.taurusta@gmail.com>
2018-03-12 22:05:48 -0700

    all

---
+++ src/containers/Message/Message.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Message extends Component {
+  render() {
+    return (
+      <div className="message ">
+        <h1> hello this is message or sermons </h1>
+        <p> have a wonderful day </p>
+      </div>
+    );
+  }
+}
